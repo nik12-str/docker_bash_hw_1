b9b56f8078faf6d5015b83090a18a70f903573d4
Nikita Streltsov <nianstreltsov@edu.hse.ru>
2026-06-12 12:41:55 +0300

    Добавил необходимые функции, выполнив задачу Аналитик данных

--- run.sh
+++ run.sh
@@ -14,8 +14,18 @@ create_local_data() {
     python generate.py local_data
 }
 
+build_reporter() {
+    docker build -f Dockerfile.reporter -t reporter-image .
+}
+
+run_reporter() {
+    MSYS_NO_PATHCONV=1 docker run --rm -v "$(pwd)/data:/data" reporter-image
+}
+
 case "$1" in
     build_generator) build_generator ;;
     run_generator) run_generator ;;
     create_local_data) create_local_data ;;
+    build_reporter) build_reporter ;;
+    run_reporter) run_reporter ;;
 esac
